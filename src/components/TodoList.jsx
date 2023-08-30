6316673526b5530fce5b761b6daab29bb4e1ae9b
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-08-30 17:25:43 +0600

    first commit

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,16 @@
+import Todo from "./Todo";
+
+
+const TodoList = () => {
+    return (
+        <div
+                class="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto"
+            >
+               <Todo></Todo>
+
+              
+            </div>
+    );
+};
+
+export default TodoList;
