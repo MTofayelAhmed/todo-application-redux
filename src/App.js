fed621ea8d4f9a14d36ad9e27e78acfa611fc860
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-08-31 16:30:33 +0600

    done with todo feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Provider } from "react-redux";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import TodoList from "./components/TodoList";
+import store from './redux/store';
 
 function App() {
   return (
+    <Provider store={store}>
     <body>
       <div class="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
         <Navbar></Navbar>
@@ -22,6 +25,7 @@ function App() {
         </div>
       </div>
     </body>
+    </Provider>
   );
 }
 
---
+++ src/redux/todos/initialState.js
@@ -0,0 +1,15 @@
+ const initialState = [
+    {
+        id: 1,
+        text: "Learned React Js",
+        completed: true,
+    },
+    {
+        id: 2,
+        text: "Learned Redux",
+        completed: false,
+        color: "red"
+    }
+]
+
+export default initialState;
