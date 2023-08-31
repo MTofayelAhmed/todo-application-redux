894af8466997ef438f184cd2ce529832e8f13bd3
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-08-31 16:48:37 +0600

    done with commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,25 @@
+import { useSelector } from "react-redux";
 
-
+const numberOfTodo  = (No_of_Todo)=> {
+    switch (No_of_Todo) {
+        case 0:
+            
+            return "No task"
+            case 1: 
+            return "1 task "
+    
+        
+        default:
+           return `${No_of_Todo} tasks`
+    }
+}
 const Footer = () => {
+    const todos = useSelector((state)=> state.todos)
+    const taskRemaining = todos.filter(todo => !todo.completed).length;
+  
     return (
         <div class="mt-4 flex justify-between text-xs text-gray-500">
-                <p>2 tasks left</p>
+                <p>{numberOfTodo(taskRemaining )} left</p>
                 <ul className="flex space-x-1 items-center text-xs">
                     <li className="cursor-pointer font-bold">All</li>
                     <li>|</li>
